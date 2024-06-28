5f75737be985344e6b91b47a863ef3cdc0eaca8c
Maximilien Lafont <maxilafxl@gmail.com>
2024-06-28 09:45:05 +0200

    API python -> app.py est prête les fichiers : - MatchCSGO.py - match_all.json - match_info.json sont complétement inutile et serve juste pour la déco

--- server/MatchCSGO.py
+++ server/MatchCSGO.py
@@ -6,7 +6,7 @@ class MatchCSGO:
         self.hltv = Hltv()
 
     async def get_live_matches(self):
-        live_matches = await self.hltv.get_matches(future=False, min_rating=0, days=0)
+        live_matches = await self.hltv.get_matches(future=False, min_rating=0, days=1)
         return live_matches
     
     async def close(self):
@@ -20,6 +20,16 @@ async def main():
     finally:
         await matchCSGO.close()
 
+async def test():
+    hltv = Hltv()
+    try:
+        matchcs = await hltv.get_matches(future=True, min_rating=0, days=1)
+        print(len(matchcs))
+        print(matchcs[0])
+        print("-----------------------")
+        print(await hltv.get_match_info(matchcs[0]["id"], matchcs[0]["team1"], matchcs[0]["team1"], matchcs[0]["event"]))
+    finally:
+        await hltv.close()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(test())
--- server/app.py
+++ server/app.py
@@ -1,28 +1,44 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from hltv_async_api import Hltv
 import asyncio
 
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/chiffre")
-def chiffre():
-    chiffre = 47
-    return jsonify(chiffre)
+hltv = Hltv()
 
-@app.route("/api/nom")
-def nom():
-    nom = "Tim"
-    return jsonify(nom)
+async def fetchLive_matches():
+    matches = await hltv.get_matches(future=False, min_rating=0, days=1)
+    return matches
 
-@app.route("/api/tout")
-def tout():
-    obj = {
-        "nom": "James",
-        "chiffre": 56
-    }
-    return jsonify(obj)
+async def fetchMatch():
+    matches_info = []
+    for match in await hltv.get_matches(future=False, min_rating=0, days=1):
+        matches = await hltv.get_match_info(match["id"], match["team1"], match["team2"], match["event"])
+        matches_info.append(matches)
+    return matches
+
+async def fetchLenmatches():
+    matchesLen = len(await hltv.get_matches(future=False, min_rating=0, days=1))
+    return matchesLen
+
+@app.route("/api/live_matches")
+async def live_matches(): 
+    dataLive_matches = asyncio.run(fetchLive_matches())
+    return jsonify(dataLive_matches)
+
+@app.route("/api/matches")
+def match_data():
+    dataMatch = asyncio.run(fetchMatch()())
+    return jsonify(dataMatch)
+
+@app.route("/api/matches_len")
+def match_data():
+    dataLenMatch = asyncio.run(fetchLenMatch()())
+    return jsonify(dataLenMatch)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
